16b566c83795981bf3731f1bb7aacfe2ceb6fd5b
Marwin2002 <musicbotid@gmail.com>
2024-12-29 16:58:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # TEAM BRANDED ALL COPYRIGHT ©️
 class Config:
-    API_ID = "28461830"
-    API_HASH = "58935dd717d0205b8051e77205f407fb"
-    TOKEN = "7041601015:AAGIiC7ufdA3s9ot4ODoOC1y-q1nO7Lp31g"
-    START_PIC = "https://te.legra.ph/file/e1b4148d7a4cd8b0832d9.jpg"
+    API_ID = "27758016"
+    API_HASH = "8d34cfffe27ab461eabbf0091b1a27df"
+    TOKEN = "7808172723:AAFCRy-9KG0m_ofcyNevtEI0fzBekSvweRU"
+    START_PIC = "https://envs.sh/5gF.jpg"
